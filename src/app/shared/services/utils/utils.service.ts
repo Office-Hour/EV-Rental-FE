502eb6fa8a5ced01969f646a0f528c68777137cc
HungLT <hungltse184143@fpt.edu.vn>
2025-10-24 15:26:25 +0700

    fix

--- src/app/shared/services/utils/utils.service.ts
+++ src/app/shared/services/utils/utils.service.ts
@@ -40,11 +40,11 @@ export class SharedUtilsService {
    *
    * @param length
    */
-  randomId(length: number = 10): string {
+  randomId(length: number = 20): string {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let name = '';
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < length; i++) {
       name += chars.charAt(Math.floor(Math.random() * chars.length));
     }
 
