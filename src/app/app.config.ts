2ab76e2b3d2fb2231cf311654db2bbeea71e7c95
nnh53 <nnh53.work@gmail.com>
2025-10-25 21:50:28 +0700

    feat: remove old scrollbar, install ngx-scrollbar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideCacheableAnimationLoader, provideLottieOptions } from 'ngx-lottie';
 import { provideCommonUI } from './common-ui/provider';
+import { provideScrollbarOptions } from 'ngx-scrollbar';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+
+    // https://ngx-scrollbar.netlify.app/#/
+    provideScrollbarOptions({
+      visibility: 'hover',
+      appearance: 'compact',
+    }),
 
     //Common UI
     provideCommonUI(),
