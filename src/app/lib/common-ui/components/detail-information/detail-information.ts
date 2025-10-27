9a8526bfbf185ea3c11e0eddc9e78e4e06362a78
HungLT <hungltse184143@fpt.edu.vn>
2025-10-27 21:01:25 +0700

    feat: update

---
+++ src/app/lib/common-ui/components/detail-information/detail-information.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-information',
+  imports: [],
+  templateUrl: './detail-information.html',
+  styleUrl: './detail-information.scss',
+})
+export class DetailInformation {}
