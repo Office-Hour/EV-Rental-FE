85624120242a16078ff0a0fd6e1a380aa16b7ba3
HungLT <hungltse184143@fpt.edu.vn>
2025-10-28 19:34:33 +0700

    feat: layout update

---
+++ src/app/features/booking/booking.route.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CarList } from './pages/car-list/car-list';
+
+export default [
+  {
+    path: '',
+    component: CarList,
+  },
+] as Routes;
---
+++ src/app/lib/common-ui/components/comfirmation-dialog/comfirmation-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comfirmation-dialog',
+  imports: [],
+  templateUrl: './comfirmation-dialog.html',
+  styleUrl: './comfirmation-dialog.scss',
+})
+export class ComfirmationDialog {}
