346b8fe72a56c075d9f617e479523e1875e31a62
HungLT <hungltse184143@fpt.edu.vn>
2025-11-06 14:45:04 +0700

    fix: api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideApi({
       basePath:
-        'https://swd-be-webapi-production.politerock-c1f6f715.japaneast.azurecontainerapps.io/',
+        'https://swd-be-webapi-production.politerock-c1f6f715.japaneast.azurecontainerapps.io',
     }),
 
     APIS,
