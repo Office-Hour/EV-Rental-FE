ec5ba42a868dcb8d925014eccff3969313b3dbe6
nnh53 <nnh53.work@gmail.com>
2025-11-07 04:58:13 +0700

    fix: rename

---
+++ src/app/features/staff/staff-dashboard/staff-dashboard.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-dashboard',
+  imports: [],
+  templateUrl: './staff-dashboard.html',
+  styleUrl: './staff-dashboard.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StaffDashboard {}
